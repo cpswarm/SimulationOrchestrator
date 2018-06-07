ade67296334189c53bf9a424f25869b3072952a4
Davide Conzon <conzon@ismb.it>
2018-06-07 10:35:39 +0200

    Added the listener to receive the results from the Optimization Tool

---
+++ src/main/java/simulation/xmpp/FileTransferListenerImpl.java
@@ -0,0 +1,45 @@
+package simulation.xmpp;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.jivesoftware.smack.SmackException;
+import org.jivesoftware.smackx.filetransfer.FileTransferListener;
+import org.jivesoftware.smackx.filetransfer.FileTransferRequest;
+import org.jivesoftware.smackx.filetransfer.IncomingFileTransfer;
+
+import simulation.SimulationOrchestrator;
+
+import org.jivesoftware.smackx.filetransfer.FileTransfer.Status;
+
+
+public abstract class FileTransferListenerImpl implements FileTransferListener {
+
+	protected SimulationOrchestrator parent = null;
+	protected String dataFolder = null;
+	
+	public FileTransferListenerImpl(final SimulationOrchestrator orchestrator) {
+		this.parent = orchestrator;
+	}
+	
+	@Override
+	public void fileTransferRequest(FileTransferRequest request) {
+		final IncomingFileTransfer transfer = request.accept();
+		final String fileToReceive = dataFolder+request.getFileName();
+		try {
+			transfer.recieveFile(new File(fileToReceive));
+
+			while (!transfer.isDone()) {
+				if (transfer.getStatus() == Status.refused) {
+					System.out.println("Transfer refused");
+				}
+				Thread.sleep(1000);
+			}
+			System.out.println("File received");
+			Thread.sleep(1000);
+			//TODO Confirm the optimization results
+		} catch (final SmackException | IOException | InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
