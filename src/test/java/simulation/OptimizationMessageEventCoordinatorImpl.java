5f6b6b1389d134ae4641b380c155f39b1ab0383a
Davide Conzon <conzon@ismb.it>
2018-07-03 20:28:15 +0200

    Added the OptimizationProgress

--- src/test/java/simulation/OptimizationMessageEventCoordinatorImpl.java
+++ src/test/java/simulation/OptimizationMessageEventCoordinatorImpl.java
@@ -8,6 +8,7 @@ import org.jxmpp.jid.EntityBareJid;
 import com.google.gson.Gson;
 
 import messages.progress.GetProgress;
+import messages.progress.OptimizationProgress;
 import messages.reply.OptimizationReply;
 import messages.start.StartOptimization;
 
@@ -22,28 +23,36 @@ public final class OptimizationMessageEventCoordinatorImpl implements IncomingCh
 	
 	@Override
 	public void newIncomingMessage(EntityBareJid jid, Message msg, org.jivesoftware.smack.chat2.Chat chat) {
-		OptimizationReply reply = new OptimizationReply();
 		Gson gson = new Gson();
+		Message message = new Message();
 		if(msg.getBody().contains("Start")) {
 			StartOptimization start = gson.fromJson(msg.getBody(), StartOptimization.class);
+			OptimizationReply reply = new OptimizationReply();
 			reply.setID(start.getID());
 			reply.setTitle(OptimizationReply.OPTIMIZATION_STARTED);
 			reply.setOperationStatus("OK");
+			message.setBody(gson.toJson(reply));
+			try {
+				chat.send(message);
+			} catch (NotConnectedException | InterruptedException e) {
+				System.out.println("Error sending the reply");
+				e.printStackTrace();
+			}
 		} else if(msg.getBody().contains("Get")) {
 			GetProgress getProgress = gson.fromJson(msg.getBody(), GetProgress.class);
 			value +=10;
-			reply.setID(getProgress.getID());
-			reply.setTitle(OptimizationReply.OPTIMIZATION_PROGRESS);
-			reply.setOperationStatus(value + "%");
-			
+			OptimizationProgress progress = new OptimizationProgress();
+			progress.setID(getProgress.getID());
+			progress.setOperationStatus(String.valueOf(value));
+			progress.setUom("%");
+			message.setBody(gson.toJson(progress));
+			try {
+				chat.send(message);
+			} catch (NotConnectedException | InterruptedException e) {
+				System.out.println("Error sending the progress");
+				e.printStackTrace();
+			}
 		}
-		Message message = new Message();
-		message.setBody(gson.toJson(reply));
-		try {
-			chat.send(message);
-		} catch (NotConnectedException | InterruptedException e) {
-			System.out.println("Error sending the reply");
-			e.printStackTrace();
-		}
+		
 	}
 }
