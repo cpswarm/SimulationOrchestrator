46aaedacab29768b98d808720fe8328d8ee60df3
Davide Conzon <conzon@ismb.it>
2018-08-24 13:37:15 +0200

    Added comments to the test

--- src/test/java/simulation/AppTest.java
+++ src/test/java/simulation/AppTest.java
@@ -27,6 +27,28 @@ import simulation.SimulationOrchestrator;
  * Unit test for simple App.
  */
 public class AppTest extends TestCase{
+	/**
+	 * Test running example
+	 * 
+	 * mvn test 
+	 * -Dtest_server_ip=130.192.86.237 (IP of the XMPP server) 
+	 * -Dtest_server_name=pert-demoenergy-virtus.ismb.polito.it (name of the XMPP server) 
+	 * -Dtest_server_password=orchestrator (Password to be used by the orchestrator to authenticate in the XMPP server)
+	 * -Dtest_orchestrator_input_data_folder=/home/cpswarm/Desktop/cpswarm/ (folder containing the input files)
+	 * -Dtest_orchestrator_output_data_folder=/home/cpswarm/Desktop/cpswarm-out (folder where the output files will be inserted)
+	 * -Dtest_manager_data_folder=/home/cpswarm/Desktop/output/ (data folder used by the simulation manager) 
+	 * -Dtest_manager2_data_folder=/home/cpswarm/Desktop/output2/ (data folder used by the second simulation manager for multiple simulations test)
+	 * -Doptimization_user=optimization_test (XMPP username used for the Optimization Tool)
+	 * -Dot_data_folder=/home/cpswarm/Desktop/ot/  (folder used by the Optimization Tool)
+	 * -Dros_folder=/home/cpswarm/Desktop/test/src/emergency_exit/src/ (Folder used for the ROS package to start the first simulation)
+	 * -Dros2_folder=/home/cpswarm/Desktop/test2/src/emergency_exit/src/ (Folder used for the ROS package to start the second simulation for multiple simulations test)
+	 * -Dmonitoring=true (indicates if the monitoring GUI has to be used, monitoring the evolution of the optimization)
+	 * -Dmqtt_broker=tcp://130.192.86.237:1883  (IP of the MQTT broker to be used for the monitoring)
+	 * -Dgui_enabled=false (indicates if the GUI has to be used during the simulations)
+	 * -Doptimization_id=emergency_exit (ID of the optimization AKA the name of the package)
+	 * -Djavax.xml.accessExternalDTD=all (configuration for xml parsing)
+	 * 
+	 */
 	private String serverIP = System.getProperty("test_server_ip");
 	private String serverName = System.getProperty("test_server_name");
 	private String serverPassword = System.getProperty("test_server_password");
