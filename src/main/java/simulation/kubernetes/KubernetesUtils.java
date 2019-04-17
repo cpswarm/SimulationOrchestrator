df8092e60d3e1b5e50f0610f137d14a1f28c881b
Davide Conzon <conzon@ismb.it>
2019-04-17 10:05:16 +0200

    Updated the Kubernetes Utils to check the deployed replicas

--- src/main/java/simulation/kubernetes/KubernetesUtils.java
+++ src/main/java/simulation/kubernetes/KubernetesUtils.java
@@ -12,7 +12,6 @@ import config.deployment.Deployment;
 import io.kubernetes.client.ApiClient;
 import io.kubernetes.client.ApiException;
 import io.kubernetes.client.Configuration;
-import io.kubernetes.client.apis.CoreV1Api;
 import io.kubernetes.client.apis.ExtensionsV1beta1Api;
 import io.kubernetes.client.models.ExtensionsV1beta1Deployment;
 import io.kubernetes.client.models.ExtensionsV1beta1DeploymentSpec;
@@ -38,75 +37,100 @@ public final class KubernetesUtils {
 	 * @return true if all is OK, false otherwise
 	 */
 	public static boolean deploy(Deployment deploy) {
-		ExtensionsV1beta1Deployment deployment = new ExtensionsV1beta1Deployment(); 
-		deployment.setKind("Deployment");
-		deployment.setApiVersion("extensions/v1beta1");
-
-		V1ObjectMeta meta = new V1ObjectMeta();
-		meta.setName(deploy.getMetadata().getName());
-		meta.setNamespace(deploy.getMetadata().getNamespace());
-		meta.setSelfLink("/apis/extensions/v1beta1/namespaces/default/deployments/"+deploy.getMetadata().getName());
-		meta.setUid(UUID.randomUUID().toString());
-		meta.setGeneration(Long.valueOf(1));
-		Map<String,String> labelsMap = new HashMap<String,String>();
-		labelsMap.put("k8s-app", deploy.getMetadata().getLabels().getK8sApp());
-		meta.setLabels(labelsMap);
-		Map<String,String> annotationsMap = new HashMap<String,String>();
-		annotationsMap.put("deployment.kubernetes.io/revision", "1");
-		meta.setAnnotations(annotationsMap);
-		deployment.setMetadata(meta);
-
-		ExtensionsV1beta1DeploymentSpec spec = new ExtensionsV1beta1DeploymentSpec();
-		spec.setReplicas(deploy.getSpec().getReplicas());
-		V1LabelSelector labelSelector = new V1LabelSelector();
-		labelSelector.putMatchLabelsItem("k8s-app", deploy.getSpec().getSelector().getMatchLabels().getK8sApp());
-		spec.setSelector(labelSelector);
-		V1PodTemplateSpec template = new V1PodTemplateSpec();
-		V1ObjectMeta podMeta = new V1ObjectMeta();
-		podMeta.setName(deploy.getTemplate().getMetadata().getName());
-		podMeta.setLabels(labelsMap);
-		template.setMetadata(podMeta);
-		spec.setTemplate(template);
-		V1PodSpec podSpec = new V1PodSpec();
-		List<V1Container> containersList = new ArrayList<V1Container>();
-		for (Container c : deploy.getTemplate().getSpec().getContainers()) {
-			V1Container container = new V1Container();
-			container.setName(c.getName());
-			container.setImage(c.getImage());
-			container.setTerminationMessagePath("/dev/termination-log");
-			container.setTerminationMessagePolicy("File");
-			container.setImagePullPolicy("IfNotPresent");
-			V1SecurityContext secContext = new V1SecurityContext();
-			secContext.setPrivileged(false);
-			container.setSecurityContext(secContext);
-			if(c.getArgs()!=null) {
-				container.setArgs(c.getArgs());
+		ExtensionsV1beta1Deployment currentDeployment = null;
+		ExtensionsV1beta1Deployment deploymentToDo = new ExtensionsV1beta1Deployment(); 
+		try {
+			// Check the current status of the deployment
+			ApiClient client = Config.defaultClient();
+			Configuration.setDefaultApiClient(client);
+			ExtensionsV1beta1Api extsApi = new ExtensionsV1beta1Api();
+			try {
+				currentDeployment = extsApi.readNamespacedDeploymentStatus(deploy.getMetadata().getName(), deploy.getMetadata().getNamespace(), "true");
+			} catch(ApiException ex) {
+				if(ex.getCode()!=404) {
+					System.out.println("Exception checking the current deployment status for "+deploy.getMetadata().getName());
+					return false;
+				}
 			}
-			containersList.add(container);
-		}
-		Map<String,String> nodeSelector = new HashMap<String,String>();
-		nodeSelector.put("component", deploy.getTemplate().getSpec().getNodeSelector().getComponent());
-		podSpec.setNodeSelector(nodeSelector);
-		podSpec.setContainers(containersList);
-		podSpec.setRestartPolicy("Always");
-		podSpec.setTerminationGracePeriodSeconds(Long.valueOf(30));
-		podSpec.setDnsPolicy("ClusterFirst");
-		V1PodSecurityContext podSecContext = new V1PodSecurityContext();
-		podSpec.setSecurityContext(podSecContext);
-		podSpec.setSchedulerName("default-scheduler");
-		template.setSpec(podSpec);
-		ExtensionsV1beta1DeploymentStrategy strategy = new ExtensionsV1beta1DeploymentStrategy();
-		strategy.setType("RollingUpdate");
-		ExtensionsV1beta1RollingUpdateDeployment rollingUpdate = new ExtensionsV1beta1RollingUpdateDeployment();
-		rollingUpdate.setMaxUnavailable("25%");
-		rollingUpdate.setMaxSurge("25%");
-		strategy .setRollingUpdate(rollingUpdate);
-		spec.setStrategy(strategy);
-		spec.setRevisionHistoryLimit(10);
-		spec.setProgressDeadlineSeconds(600);
-		deployment.setSpec(spec);
+			if(currentDeployment!=null) {
+				// If something is already deployed 
+				// but the current number of replica is not equal to the desired one
+				// it scales the current deployment to reach the desired status
+				if(!currentDeployment.getStatus().getAvailableReplicas().equals(deploy.getSpec().getReplicas())) {
+					KubernetesUtils.scale(currentDeployment.getMetadata(), deploy.getSpec().getReplicas());
+				} else {
+					return true;
+				}
+			}
+			
+			deploymentToDo.setKind("Deployment");
+			deploymentToDo.setApiVersion("extensions/v1beta1");
 
-		/*
+			V1ObjectMeta meta = new V1ObjectMeta();
+			meta.setName(deploy.getMetadata().getName());
+			meta.setNamespace(deploy.getMetadata().getNamespace());
+			meta.setSelfLink("/apis/extensions/v1beta1/namespaces/default/deployments/"+deploy.getMetadata().getName());
+			meta.setUid(UUID.randomUUID().toString());
+			meta.setGeneration(Long.valueOf(1));
+			Map<String,String> labelsMap = new HashMap<String,String>();
+			labelsMap.put("k8s-app", deploy.getMetadata().getLabels().getK8sApp());
+			meta.setLabels(labelsMap);
+			Map<String,String> annotationsMap = new HashMap<String,String>();
+			annotationsMap.put("deployment.kubernetes.io/revision", "1");
+			meta.setAnnotations(annotationsMap);
+			deploymentToDo.setMetadata(meta);
+
+			ExtensionsV1beta1DeploymentSpec spec = new ExtensionsV1beta1DeploymentSpec();
+			spec.setReplicas(deploy.getSpec().getReplicas());
+			V1LabelSelector labelSelector = new V1LabelSelector();
+			labelSelector.putMatchLabelsItem("k8s-app", deploy.getSpec().getSelector().getMatchLabels().getK8sApp());
+			spec.setSelector(labelSelector);
+			V1PodTemplateSpec template = new V1PodTemplateSpec();
+			V1ObjectMeta podMeta = new V1ObjectMeta();
+			podMeta.setName(deploy.getTemplate().getMetadata().getName());
+			podMeta.setLabels(labelsMap);
+			template.setMetadata(podMeta);
+			spec.setTemplate(template);
+			V1PodSpec podSpec = new V1PodSpec();
+			List<V1Container> containersList = new ArrayList<V1Container>();
+			for (Container c : deploy.getTemplate().getSpec().getContainers()) {
+				V1Container container = new V1Container();
+				container.setName(c.getName());
+				container.setImage(c.getImage());
+				container.setTerminationMessagePath("/dev/termination-log");
+				container.setTerminationMessagePolicy("File");
+				container.setImagePullPolicy("IfNotPresent");
+				V1SecurityContext secContext = new V1SecurityContext();
+				secContext.setPrivileged(false);
+				container.setSecurityContext(secContext);
+				if(c.getArgs()!=null) {
+					container.setArgs(c.getArgs());
+				}
+				containersList.add(container);
+			}
+			Map<String,String> nodeSelector = new HashMap<String,String>();
+			nodeSelector.put("component", deploy.getTemplate().getSpec().getNodeSelector().getComponent());
+			podSpec.setNodeSelector(nodeSelector);
+			podSpec.setContainers(containersList);
+			podSpec.setRestartPolicy("Always");
+			podSpec.setTerminationGracePeriodSeconds(Long.valueOf(30));
+			podSpec.setDnsPolicy("ClusterFirst");
+			V1PodSecurityContext podSecContext = new V1PodSecurityContext();
+			podSpec.setSecurityContext(podSecContext);
+			podSpec.setSchedulerName("default-scheduler");
+			template.setSpec(podSpec);
+			ExtensionsV1beta1DeploymentStrategy strategy = new ExtensionsV1beta1DeploymentStrategy();
+			strategy.setType("RollingUpdate");
+			ExtensionsV1beta1RollingUpdateDeployment rollingUpdate = new ExtensionsV1beta1RollingUpdateDeployment();
+			rollingUpdate.setMaxUnavailable("25%");
+			rollingUpdate.setMaxSurge("25%");
+			strategy .setRollingUpdate(rollingUpdate);
+			spec.setStrategy(strategy);
+			spec.setRevisionHistoryLimit(10);
+			spec.setProgressDeadlineSeconds(600);
+			deploymentToDo.setSpec(spec);
+
+			/*
 		ExtensionsV1beta1DeploymentStatus status = new ExtensionsV1beta1DeploymentStatus();
 		status.setObservedGeneration(Long.valueOf(1));
 		status.setReplicas(1);
@@ -132,19 +156,14 @@ public final class KubernetesUtils {
 		conditions.add(availableCondition);
 		status.setConditions(conditions);
 		deployment.setStatus(status);
-		*/
-		
-		try {
-			ApiClient client = Config.defaultClient();
-			Configuration.setDefaultApiClient(client);
+			 */
 
-			ExtensionsV1beta1Api extsApi = new ExtensionsV1beta1Api();
-			ExtensionsV1beta1Deployment result = extsApi.createNamespacedDeployment("default", deployment, "true");
+			ExtensionsV1beta1Deployment result = extsApi.createNamespacedDeployment("default", deploymentToDo, "true");
 			io.kubernetes.client.JSON json = new io.kubernetes.client.JSON();
 			String resultJson = json.serialize(result);
 			System.out.println(resultJson);
 		} catch (ApiException | IOException e) {
-			System.out.println("Error deploying a simulator "+deployment.getMetadata().getName());
+			System.out.println("Error deploying a simulator "+deploymentToDo.getMetadata().getName());
 			e.printStackTrace();
 			return false;
 		}
@@ -164,7 +183,7 @@ public final class KubernetesUtils {
 		try {
 			ApiClient client = Config.defaultClient();
 			Configuration.setDefaultApiClient(client);
-			ExtensionsV1beta1Scale result = extsApi.replaceNamespacedDeploymentScale("test", "default", body, "true");
+			ExtensionsV1beta1Scale result = extsApi.replaceNamespacedDeploymentScale(metadata.getName(), metadata.getNamespace(), body, "true");
 			io.kubernetes.client.JSON json = new io.kubernetes.client.JSON();
 			String resultJson = json.serialize(result);
 			System.out.println(resultJson);			
